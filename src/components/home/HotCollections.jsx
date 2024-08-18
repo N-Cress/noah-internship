62a22f951e4f283a4ed3db3cf983dfcb30aab354
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-08-18 11:13:02 -0600

    HotCollections.jsx now retrieves from api resources with axios and that retrieved data is displayed. Carousel implemented for this content with responsive design

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
+  const [dataSet, setDataSet] = useState([]);
+  let data;
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 756,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
+
+  useEffect((
+  ) => {
+    async function getData() {
+      ({data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"))
+      setDataSet(data)
+    }
+    getData()
+    
+  }, [])
+  console.log(dataSet)
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +61,32 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          <Slider {...settings}>
+          { !!dataSet ? dataSet.map((obj) => (
+            <div className="col"key={obj.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/` + obj.nftId}>
+                    <img src={obj.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                  <Link to={`/author/` + obj.authorId}>
+                    <img className="lazy pp-coll" src={obj.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{obj.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{obj.code}</span>
                 </div>
               </div>
-            </div>
-          ))}
+            </div> 
+          )) : <div> Hello </div>}
+          </Slider>
+         
         </div>
       </div>
     </section>
