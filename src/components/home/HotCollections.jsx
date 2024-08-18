4d75843e068801cc76140ff44e3db3083113eb15
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-08-18 14:23:26 -0600

    changed false value back to dataSet as intended for ternary in Hot Collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -61,7 +61,7 @@ const HotCollections = () => {
             </div>
           </div>
           <Slider {...settings}>
-          { false ? dataSet.map((obj) => (
+          { !!dataSet ? dataSet.map((obj) => (
             <div className="col"key={obj.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
