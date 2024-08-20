7ef07904c3fd374ba65f4ad0fb48f383c7722854
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-08-19 20:43:19 -0600

    skeleton loading states for NewItems

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -3,10 +3,43 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const NewItems = () => {
   const [dataSet, setDataSet] = useState([]);
   const [timeLeft, setTimeLeft] = useState({});
+
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 756,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
 
   useEffect(() => {
     async function getData() {
@@ -63,9 +96,9 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {dataSet.length
-            ? dataSet.map((obj) => (
-                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={obj.id}>
+          <Slider {...settings}>
+          {dataSet ? dataSet.map((obj) => (
+                <div className="col" key={obj.id}>
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
@@ -105,7 +138,7 @@ const NewItems = () => {
                         </div>
                       </div>
 
-                      <Link to="/item-details">
+                      <Link to={`/item-details/${obj.nftId}`}>
                         <img
                           src={obj.nftImage}
                           className="lazy nft__item_preview"
@@ -114,7 +147,7 @@ const NewItems = () => {
                       </Link>
                     </div>
                     <div className="nft__item_info">
-                      <Link to="/item-details">
+                      <Link to={`/item-details/${obj.nftId}`}>
                         <h4>{obj.title}</h4>
                       </Link>
                       <div className="nft__item_price">{obj.price}</div>
@@ -127,22 +160,12 @@ const NewItems = () => {
                 </div>
               ))
             : new Array(4).fill(0).map((_, index) => (
-                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="col" key={index}>
                   <div className="nft__item">
-                    <div className="author_list_pp">
-                      <Link
-                        to="/author"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        title="Creator: Monica Lucas"
-                      >
-                        <img className="lazy" src={AuthorImage} alt="" />
+                    <div className="lazy-author_pp author_list_pp">
                         <i className="fa fa-check"></i>
-                      </Link>
                     </div>
-                    <div className="de_countdown">5h 30m 32s</div>
-
-                    <div className="nft__item_wrap">
+                    <div className="lazy-nft__item_wrap nft__item_wrap">
                       <div className="nft__item_extra">
                         <div className="nft__item_buttons">
                           <button>Buy Now</button>
@@ -160,28 +183,16 @@ const NewItems = () => {
                           </div>
                         </div>
                       </div>
-
-                      <Link to="/item-details">
-                        <img
-                          src={nftImage}
-                          className="lazy nft__item_preview"
-                          alt=""
-                        />
-                      </Link>
                     </div>
                     <div className="nft__item_info">
-                      <Link to="/item-details">
-                        <h4>Pinky Ocean</h4>
-                      </Link>
-                      <div className="nft__item_price">3.08 ETH</div>
-                      <div className="nft__item_like">
-                        <i className="fa fa-heart"></i>
-                        <span>69</span>
-                      </div>
+                    <div className="lazy-item_title"> </div>
+                      <div className="nft__item_price lazy-item_price"> </div>
+                      <div className="nft__item_like lazy-item_like"> </div>
                     </div>
                   </div>
                 </div>
               ))}
+              </Slider>
         </div>
       </div>
     </section>
