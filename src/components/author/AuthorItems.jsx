9601b623840f83b19ddfdbda0c94628791c010c3
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-08-21 18:08:22 -0600

    Revert "author and author items are dynamically presenting data based on route and lazy loading"
    This reverts commit d9f59b33bb91a5a77e962038b7f5b0871f6e6d47.

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,60 +1,22 @@
-import React, { useEffect, useState} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import AuthorImage from "../../images/author_thumbnail.jpg";
+import nftImage from "../../images/nftImage.jpg";
 
-
-const AuthorItems = ( {nftCollection, authorImage, authorId} ) => {
-
+const AuthorItems = () => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {nftCollection ? nftCollection.map((obj) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={obj.id}>
-                <div className="nft__item">
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <Link to={`/item-details/` + obj.nftId}>
-                      <img
-                        src={obj.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to={`/item-details/` + obj.nftId}>
-                      <h4>{obj.title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{obj.price} ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{obj.likes}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          :
-          new Array(8).fill(0).map((_, index) => (
+          {new Array(8).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="lazy-nft__item nft__item">
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Link to="">
+                    <img className="lazy" src={AuthorImage} alt="" />
+                    <i className="fa fa-check"></i>
+                  </Link>
+                </div>
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
                     <div className="nft__item_buttons">
@@ -72,6 +34,23 @@ const AuthorItems = ( {nftCollection, authorImage, authorId} ) => {
                         </a>
                       </div>
                     </div>
+                  </div>
+                  <Link to="/item-details">
+                    <img
+                      src={nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Link>
+                </div>
+                <div className="nft__item_info">
+                  <Link to="/item-details">
+                    <h4>Pinky Ocean</h4>
+                  </Link>
+                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>97</span>
                   </div>
                 </div>
               </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,31 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import AuthorImage from "../images/author_thumbnail.jpg";
 
 const Author = () => {
-  let { userId } = useParams();
-  const [dataSet, setDataSet] = useState(false);
-  const [follow, setFollow] = useState(false);
-
-  function followButton() {
-    setFollow(!follow);
-  }
-
-  useEffect(() => {
-    let data;
-    async function getData() {
-      ({data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=" + userId));
-      setDataSet(data)
-    }
-    getData()
-  }, [dataSet, follow])
-  
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
+
         <section
           id="profile_banner"
           aria-label="section"
@@ -33,23 +17,23 @@ const Author = () => {
           data-bgimage="url(images/author_banner.jpg) top"
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
-        <section aria-label="section"> 
+
+        <section aria-label="section">
           <div className="container">
             <div className="row">
-              {dataSet ?
               <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={dataSet.authorImage} alt="" />
+                      <img src={AuthorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          {dataSet.authorName}
-                          <span className="profile_username">{dataSet.tag}</span>
+                          Monica Lucas
+                          <span className="profile_username">@monicaaaa</span>
                           <span id="wallet" className="profile_wallet">
-                            {dataSet.address}
+                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -60,51 +44,22 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      {follow ? <div className="profile_follower"> {dataSet.followers + 1}</div> : <div className="profile_follower"> {dataSet.followers}</div>}
+                      <div className="profile_follower">573 followers</div>
                       <Link to="#" className="btn-main">
-                        {!follow ? <div onClick={followButton}> Follow </div> : <div onClick={followButton}> Unfollow </div>}
+                        Follow
                       </Link>
                     </div>
                   </div>
                 </div>
-              </div> 
-                : <div className="col">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <div className="lazy-profile_pp"> </div>
-                      <div className="profile_name">
-                        <h4>
-                          <div className="lazy-profile_name"></div>
-                          <span className="profile_username lazy-profile_tag"> </span>
-                          <span id="wallet" className="profile_wallet lazy-profile_wallet">
-                            
-                          </span>
-                    
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex lazy-profile_follow">
-                    <div className="de-flex-col">
-                    </div>
-                  </div>
-                </div>
-              </div> 
-                }
+              </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems 
-                  nftCollection={dataSet.nftCollection}
-                  authorImage={dataSet.authorImage}
-                  authorId = {dataSet.authorId}
-                  />
+                  <AuthorItems />
                 </div>
               </div>
             </div>
           </div>
-          
         </section>
       </div>
     </div>
